8eaf418c84aa8de09d1a2c8f452d0296e9ec2951
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2022-12-17 18:55:38 +0800

    dfsf
    sdfdsf

---
+++ 12-17/app.js
@@ -0,0 +1,62 @@
+array1 = [1,0,2,3,4];
+array2 = [3,5,6,7,8,13];
+var t = array1.concat(array2)
+// function niilber(arg){
+//     var sum=0;
+//     for(var i = 0; i < arg.length; i++){
+//         sum+=i
+//       }
+//       return sum
+// }
+// var respons = niilber(array1);
+// console.log(respons);
+// var sum = 0
+// var res = array1.forEach(function(element,index,array){
+//     return sum+=index
+// });
+
+// var too = [10,20,10,40,50,60,70];
+// var target = 50;
+// function find(arg,arg1){
+//     var t = ""
+//     for(var i = 0 ; i<arg.length; i++){
+//         if(arg[i]+arg[i+1]==arg1){
+//             t+=i+" "+(i+1);
+//         }
+//     }
+//     return t
+// }
+//  var respons = find(too,target);
+
+//  console.log(respons)
+// var arr = [2, 5, 9, 6]; 
+// var target = 5;
+// function Delete(arg1,arg2){
+//     for(var i = 0; i<arg1.length;i++){
+//         if(arg1[i]==arg2){
+//          arg1.splice(i,1)
+         
+//         }
+//     }
+//     return arg1;
+// }
+// var respons = Delete(arr,target);
+// console.log(respons);
+// function Delete(arg1,arg2){
+// var t = arg1.filter((el)=>{
+//     return el != arg2
+// })
+// return t
+// }
+// var respons = Delete(arr,target);
+// console.log(respons);
+
+// function newarr(arg){
+//     var t = arg.map((el,index)=>{
+//         return el*index
+//     })
+//     return t
+// }
+// var respons = newarr(arr);
+// console.log(respons);
+
